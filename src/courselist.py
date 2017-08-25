eac6612c7fe0917fe5066cfd2bdba37ff4172afc
DingJi <dingji97@outlook.com>
2017-08-24 22:49:12 -0500

    Update courselist.py

--- src/courselist.py
+++ src/courselist.py
@@ -1,9 +1,5 @@
-courselist = [{'department': 'CS', 'number': 225, 'crn': 35917},
-              {'department': 'CS', 'number': 225, 'crn': 35919},
-              {'department': 'CS', 'number': 357, 'crn': 53280},
-              {'department': 'CS', 'number': 357, 'crn': 63536},
-              {'department': 'CS', 'number': 233, 'crn': 63733},
-              {'department': 'CS', 'number': 233, 'crn': 64513},
-              {'department': 'CS', 'number': 420, 'crn': 53685},
+courselist = [{'department': 'MATH', 'number': 417, 'crn': 65075},
+              {'department': 'MATH', 'number': 484, 'crn': 30809},
+              {'department': 'MATH', 'number': 347, 'crn': 34395},
              ]
 
