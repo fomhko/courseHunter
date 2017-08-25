8db9ea0f4eafc14319ec7f20f3dbbc96129261da
DingJi <dingji97@outlook.com>
2017-08-24 22:44:11 -0500

    Update passwords.py

--- passwords.py
+++ passwords.py
@@ -1,1 +1,1 @@
-details = {'email': 'jiding2@illinois.edu', 'password': 'Dj691126', 'recipient': 'jding97@icloud.com'}
+details = {'email': 'jiding2@illinois.edu', 'password': 'Dj691126', 'recipient': 'jieyuzhang1997@163.com'}
